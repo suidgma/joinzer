505e5498001bd8c49d3f4157488e8347729416a9
Marty Suidgeest <suidgma@gmail.com>
2026-04-21 14:33:25 -0700

    fix: trigger fresh Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+}
 
 export default nextConfig
